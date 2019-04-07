1a8d5e827ba91a5e28892c20b5f7c7abe5b88855
marmikc <marmik@umich.edu>
2019-04-06 20:14:07 -0400

    add lxml as argument to BeautifulSoup to suppress parsing warning

--- rmfCrawler.py
+++ rmfCrawler.py
@@ -4,34 +4,38 @@ import re
 import queue
 import sys
 
+
 def main():
-    #seed url file
+    # seed url file
     file = sys.argv[1]
 
-    #file with seed URLS
-    seedFile = open(file,"r").read()
+    # file with seed URLS
+    seedFile = open(file, "r").read()
 
-    #when count is 51 or more, gender switches to Female, total 100 professors(50 M and 50 F)
+    # when count is 51 or more, gender switches to Female, total 100
+    # professors(50 M and 50 F)
     count = 1
 
     for seedURL in seedFile.split():
         reviewFinder(seedURL, count)
         count += 1
 
+
 def reviewFinder(url, count):
-    #assign gender according to count
+    # assign gender according to count
     if count < 51:
         gender = "Male"
     else:
         gender = "Female"
 
-    #open a file to put output data in
-    outFile = open("training.data/" + gender + str(count) + ".data","w+")
+    # open a file to put output data in
+    outFile = open("training.data/" + gender + str(count) + ".data", "w+")
 
     html_page = urllib.request.urlopen(url)
-    soup = BeautifulSoup(html_page)
-    for review in soup.findAll("p", class_ = "commentsParagraph"):
-        #gets only the text part of each review and writes it to an output files
+    soup = BeautifulSoup(html_page, "lxml")
+    for review in soup.findAll("p", class_="commentsParagraph"):
+        # gets only the text part of each review and writes it to an output
+        # files
         review = review.get_text()
         outFile.write(review)
 
