25f2f518d8ce3f86825fe212d2ff1c550f82d39e
manojgollamudi <manojgo@umich.edu>
2019-04-08 11:59:08 -0400

    added more schools to crawl and made output file with data

--- commentCrawler.py
+++ commentCrawler.py
@@ -20,7 +20,25 @@ def main():
 	driver.set_page_load_timeout(30)
 	urls = ["http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=University+of+Michigan&schoolID=1258&queryoption=TEACHER",
 	"http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=Central+Michigan+University&schoolID=200&queryoption=TEACHER", 
-	"http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=Michigan+State+University&schoolID=601&queryoption=TEACHER", ]
+	"http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=Michigan+State+University&schoolID=601&queryoption=TEACHER", 
+	"http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=University+of+Central+Florida&schoolID=1082&queryoption=TEACHER",
+	"http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=Texas+A%26M+University+at+College+Station&schoolID=1003&queryoption=TEACHER",
+	"http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=The+Ohio+State+University&schoolID=724&queryoption=TEACHER",
+	"http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=Arizona+State+University&schoolID=45&queryoption=TEACHER",
+	"http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=Harvard+University&schoolID=399&queryoption=TEACHER",
+	"http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=Stanford+University&schoolID=953&queryoption=TEACHER",
+	"http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=University+of+Chicago&schoolID=1085&queryoption=TEACHER",
+	"http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=Duke+University&schoolID=1350&queryoption=TEACHER",
+	"http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=Cornell+University&schoolID=298&queryoption=TEACHER",
+	"http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=University+of+Memphis&schoolID=1386&queryoption=TEACHER",
+	"http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=Portland+State+University&schoolID=775&queryoption=TEACHER",
+	"http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=Bryn+Mawr+College&schoolID=139&queryoption=TEACHER",
+	"http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=Wake+Forest+University&schoolID=1130&queryoption=TEACHER",
+	"http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=University+of+Washington&schoolID=1530&queryoption=TEACHER",
+	"http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=Brigham+Young+University&schoolID=135&queryoption=TEACHER",
+	"http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=New+York+University&schoolID=675&queryoption=TEACHER",
+	"http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=University+of+Colorado+-+Boulder&schoolID=1087&queryoption=TEACHER",
+	"http://www.ratemyprofessors.com/search.jsp?queryBy=schoolId&schoolName=George+Mason+University&schoolID=352&queryoption=TEACHER"]
 	
 	for url in urls:
 		getTeacherNames(driver, url)
@@ -31,18 +49,23 @@ def main():
 			driver.set_page_load_timeout(30)
 			time.sleep(5)
 		exitedWithError = getTeacherComments(driver, teacherId)
-		if "gender" in teachers[teacherId]:
-			print(teachers[teacherId]["name"] + " " + teachers[teacherId]["gender"])
-			if teachers[teacherId]["gender"] == "female":
-				femaleReviews += len(teachers[teacherId]["comments"])
-			elif teachers[teacherId]["gender"] == "male":
-				maleReviews += len(teachers[teacherId]["comments"])
+		try:
+			if "gender" in teachers[teacherId] and teacherId in teachers:
+				print(teachers[teacherId]["name"] + " " + teachers[teacherId]["gender"])
+				if teachers[teacherId]["gender"] == "female":
+					femaleReviews += len(teachers[teacherId]["comments"])
+				elif teachers[teacherId]["gender"] == "male":
+					maleReviews += len(teachers[teacherId]["comments"])
+		except Exception as e:
+			print(e)
+			continue
 		
 	print("number of female reviews: " + str(femaleReviews))
 	print("number of male reviews: " + str(maleReviews))
 
 	for teacherId in teachers:
 		outfile.write("Teacher name: " + teachers[teacherId]["name"] + "\n")
+		outfile.write("School name: " + teachers[teacherId]["school"] + "\n")
 		outfile.write("Teacher gender: " + teachers[teacherId]["gender"] + "\n")
 		outfile.write("Teacher comments: \n")
 		for comment in teachers[teacherId]["comments"]:
@@ -55,6 +78,7 @@ def getTeacherNames(driver, url):
 	try:
 		driver.get(url)
 		teacherListElement = driver.find_elements_by_class_name('result-list')
+		schoolName = driver.find_element_by_class_name('school-name')
 	except:
 		print("timeout")
 		urls.append(url)
@@ -80,6 +104,8 @@ def getTeacherNames(driver, url):
 
 			teachers[teacherId] = {}
 			teachers[teacherId]["name"] = fullname
+			print(str(schoolName.get_attribute('innerHTML')))
+			teachers[teacherId]["school"] = str(schoolName.get_attribute('innerHTML'))
 			teacherIdsList.append(teacherId)
 
 
@@ -102,7 +128,7 @@ def getTeacherComments(driver, teacherId):
 		# identify gender based on pronouns
 		for element in commentsElement:
 			comment = element.get_attribute('innerHTML').strip()
-			if comment.find(" his") != -1:
+			if comment.find(" his ") != -1:
 				maleCount += 1
 			if comment.find(" her ") != -1:
 				femaleCount += 1
