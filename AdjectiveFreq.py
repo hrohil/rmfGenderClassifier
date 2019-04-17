0923ce6ae4d57c9ed62bf24f6ec0824c82234e2d
Maninder Singh <manising@umich.edu>
2019-04-17 14:38:26 -0400

    adding output of ratings csv for calculating t-test

--- AdjectiveFreq.py
+++ AdjectiveFreq.py
@@ -3,6 +3,8 @@ nltk.download('wordnet')
 from nltk.corpus import wordnet as wn
 from nltk.tokenize import word_tokenize
 import os
+import csv
+
 
 def main():
     stopwordsList = createStopwordsList()
@@ -16,8 +18,11 @@ def main():
     totalFemales = 0
     totalMales = 0
 
-    for file in os.listdir("commentCrawlerOutput"):
-        text = word_tokenize(open("commentCrawlerOutput/" +  file).read())
+    femaleRatings = []
+    maleRatings = []
+
+    for file in os.listdir("commentCrawlerOutputPreprocessed"):
+        text = word_tokenize(open("commentCrawlerOutputPreprocessed/" +  file).read())
         text = removeStopWords(text, stopwordsList)
 
         for word in text:
@@ -31,6 +36,9 @@ def main():
                             append(word, femaleLowDic)
                         else:
                             append(word, femaleHighDic)
+                        # Add to female ratings array
+                        femaleRatings.append(rating)
+
                     else:
                         avgMaleRating += float(rating)
                         totalMales += 1
@@ -38,6 +46,8 @@ def main():
                             append(word, maleLowDic)
                         else:
                             append(word, maleHighDic)
+                        # Add to male ratings array
+                        maleRatings.append(rating)
     print("Low rated F\n")
     printDescendingDict(femaleLowDic)
     print("\nHigh rated F\n")
@@ -50,6 +60,9 @@ def main():
     avgMaleRating = avgMaleRating / float(totalMales)
     print("\nAverage female rating " + str(avgFemaleRating))
     print("\nAverage male rating " + str(avgMaleRating))
+    
+    # Write ratings to CSV for calculating paired t-test
+    write_ratings_to_CSV(maleRatings, femaleRatings)
     
 def append(word, Dict):
     if word in Dict:
@@ -83,6 +96,20 @@ def createStopwordsList():
         word = word.rstrip()
         stopwordsList[word] = 1
     return stopwordsList
+
+def write_ratings_to_CSV(male_arr, female_arr):
+
+    if os.path.exists("prof_ratings.csv"):
+        os.remove("prof_ratings.csv")
+
+    with open('prof_ratings.csv', mode='w') as ratings_file:
+        ratings_writer = csv.writer(ratings_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        ratings_writer.writerow(['Male Prof Rating', 'Female Prof Rating'])
+        
+        print('Male arr length' + str(len(male_arr)))
+        print('Female arr length' + str(len(female_arr)))
+        for i in range(len(male_arr)):
+            ratings_writer.writerow([male_arr[i], female_arr[i]])
 
 if __name__ == "__main__":
     main()
