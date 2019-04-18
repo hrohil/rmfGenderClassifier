3717671ef82d5ef7fdf7431c37a30d0450bf7c0a
marmikc <marmik@umich.edu>
2019-04-18 15:21:55 -0400

    Add functionality to this analysis to see which words are used for one gender over another

--- AdjectiveFreq.py
+++ AdjectiveFreq.py
@@ -18,11 +18,17 @@ def main():
     totalFemales = 0
     totalMales = 0
 
+    maleDict = {}
+    femaleDict = {}
+    male_female_diff_dict = {}
+    female_male_diff_dict = {}
+
     femaleRatings = []
     maleRatings = []
 
     for file in os.listdir("commentCrawlerOutputPreprocessed"):
-        text = word_tokenize(open("commentCrawlerOutputPreprocessed/" +  file).read())
+        text = word_tokenize(
+            open("commentCrawlerOutputPreprocessed/" + file).read())
         text = removeStopWords(text, stopwordsList)
 
         for word in text:
@@ -36,6 +42,8 @@ def main():
                             append(word, femaleLowDic)
                         else:
                             append(word, femaleHighDic)
+
+                        append(word, femaleDict)
                         # Add to female ratings array
                         femaleRatings.append(rating)
 
@@ -46,8 +54,22 @@ def main():
                             append(word, maleLowDic)
                         else:
                             append(word, maleHighDic)
+
+                        append(word, maleDict)
                         # Add to male ratings array
                         maleRatings.append(rating)
+
+    maleDict = normalizeDict(maleDict)
+    femaleDict = normalizeDict(femaleDict)
+    male_female_diff_dict = populateDifferenceDict(maleDict, femaleDict)
+    female_male_diff_dict = populateDifferenceDict(femaleDict, maleDict)
+
+    print("Words used to describe males more than females\n")
+    printDescendingDict(male_female_diff_dict)
+    print('\n')
+    print("Words used to describe females more than males\n")
+    printDescendingDict(female_male_diff_dict)
+
     print("Low rated F\n")
     printDescendingDict(femaleLowDic)
     print("\nHigh rated F\n")
@@ -60,10 +82,36 @@ def main():
     avgMaleRating = avgMaleRating / float(totalMales)
     print("\nAverage female rating " + str(avgFemaleRating))
     print("\nAverage male rating " + str(avgMaleRating))
-    
+
     # Write ratings to CSV for calculating paired t-test
     write_ratings_to_CSV(maleRatings, femaleRatings)
-    
+
+
+def normalizeDict(a):
+    max_freq = 0
+    for key, value in a.items():
+        if value > max_freq:
+            max_freq = value
+
+    for key, value in a.items():
+        a[key] = value / max_freq
+
+    return a
+
+
+def populateDifferenceDict(a, b):
+    difference_dict = {}
+
+    for key in a:
+        to_subtract = 0
+        if key in b:
+            to_subtract = b[key]
+
+        difference_dict[key] = a[key] - to_subtract
+
+    return difference_dict
+
+
 def append(word, Dict):
     if word in Dict:
         Dict[word] += 1
@@ -75,9 +123,10 @@ def printDescendingDict(Dict):
     count = 0
     for item in sorted(Dict, key=Dict.get, reverse=True):
         print(str(item) + " " + str(Dict[item]))
-        count +=1
+        count += 1
         if count is 10:
             return
+
 
 def removeStopWords(tokenList, stopWordDict):
     newTokenList = []
@@ -97,15 +146,17 @@ def createStopwordsList():
         stopwordsList[word] = 1
     return stopwordsList
 
+
 def write_ratings_to_CSV(male_arr, female_arr):
 
     if os.path.exists("prof_ratings.csv"):
         os.remove("prof_ratings.csv")
 
     with open('prof_ratings.csv', mode='w') as ratings_file:
-        ratings_writer = csv.writer(ratings_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        ratings_writer = csv.writer(
+            ratings_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         ratings_writer.writerow(['Male Prof Rating', 'Female Prof Rating'])
-        
+
         print('Male arr length' + str(len(male_arr)))
         print('Female arr length' + str(len(female_arr)))
         for i in range(len(male_arr)):
