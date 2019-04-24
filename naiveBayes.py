28e0d370ca90e4089dde9e1942c367a89ab0f998
Saloni Mundhra <smundhra@umich.edu>
2019-04-24 14:12:26 -0500

    NB python file commented

--- naiveBayes.py
+++ naiveBayes.py
@@ -1,21 +1,29 @@
 import sys
 import os
 import math
+import pdb
 
 female = "female"
 male = "male"
+allFemaleFiles = []
+allMaleFiles = []
+# Top words for female and male boosting (weighting of terms)
+femaleBoosterWords = ["unclear", "other", "harder", "useless", "unfair", "pointless", "hot", "similar", "honest", "big", "careless", "random", "slower", "sweetest", "faster"]
+maleBoosterWords = ["just", "sure", "actual", "easier", "late", "most", "stronger", "easiest", "obvious", "worthless", "sad", "incorrect", "smartest", "strong", "linear"]
+booster = 7
+
 
 def main():
 	filesFolder = sys.argv[1]
 	allFiles = {}
-	allFiles = createFileStructures(filesFolder)
+	allFiles, allFemaleFile, allMaleFile = createFileStructures(filesFolder)
 	accurateResult = 0
 	averageWordProbsFemale = {}
 	averageWordProbsMale= {}
 	o = open("naivebayes.output", "w")
 	# Run the training/testing on all female data files
-	for i in range(0, 146):
-		curTestFile = female + str(i) + '.txt'
+	counter = 0
+	for curTestFile in allFemaleFile:
 		curClassProbs = {}
 		curWordProbs = {}
 		curClassProbs, curWordProbs, totalUniqueVocab, femaleCounter, maleCounter = trainNaiveBayes(allFiles, curTestFile)
@@ -35,10 +43,8 @@ def main():
 		# If result is correct, add one accuracy
 		if result == female:
 			accurateResult += 1
-
 	# Run the training/testing on all male data files
-	for i in range(0, 146):
-		curTestFile = male + str(i) + '.txt'
+	for curTestFile in allMaleFile:
 		curClassProbs = {}
 		curWordProbs = {}
 		curClassProbs, curWordProbs, totalUniqueVocab, femaleCounter, maleCounter = trainNaiveBayes(allFiles, curTestFile)
@@ -59,43 +65,58 @@ def main():
 		if result == male:
 			accurateResult += 1
 
-	#I have calculated the average top 10 words across all training sets
+	#Calculate the average top 10 words across all training sets
 	for word in averageWordProbsFemale:
-		averageWordProbsFemale[word] = float(averageWordProbsFemale[word]) / float(292)
+		averageWordProbsFemale[word] = float(averageWordProbsFemale[word]) / float(len(allFemaleFile))
 	for word in averageWordProbsMale:
-		averageWordProbsMale[word] = float(averageWordProbsMale[word]) / float(292)
+		averageWordProbsMale[word] = float(averageWordProbsMale[word]) / float(len(allMaleFile))
 	
 	print("Female Class Top 10 Words")
 	find10TopWords(averageWordProbsFemale)
 	print("Male Class Top 10 Words")
 	find10TopWords(averageWordProbsMale)
 
-	accuracy = float(accurateResult) / float(292)
+	accuracy = float(accurateResult) / float(len(allFemaleFile) + len(allMaleFile))
+	print("Booster level: " + str(booster))
 	print("Accuracy: " + str(accuracy))
 
 
 
 def createFileStructures(folder):
-	"""Input name of folder. Output dictionary of all files' content."""
+	"""Input name of folder. Output dictionary of all files' content for use later on."""
 	allFiles = {}
+
 	for filename in os.listdir(folder):
 		with open(os.path.join(folder, filename)) as content:
 			allFiles.update( {filename : content.read()} )
-		#print allFiles
-	return allFiles
+			if 'F' in filename:
+				allFemaleFiles.append(filename)
+			else:
+				allMaleFiles.append(filename)
+	allFemaleFiles.sort()
+	allMaleFiles.sort()
+
+	newFemaleFiles = ['']*103
+	newMalesFiles = ['']*103
+	for i in range(0, 103):
+		newFemaleFiles[i] = allFemaleFiles[i]
+		newMalesFiles[i] = allMaleFiles[i]
+
+	return allFiles, newFemaleFiles, newMalesFiles
 
 
 def trainNaiveBayes(allFiles, omitFile):
+	"""Function to train files using NB Classifier."""
 	classProbabilities = {}
 	wordProbabilities = []
 
 	# Create class probabilities depending on the file you're omitting
-	if 'female' in omitFile:
-		classProbabilities[female] = math.log(float(145)/float(292), 10)
-		classProbabilities[male] = math.log(float(146)/float(292), 10)
+	if 'F' in omitFile:
+		classProbabilities[female] = math.log(float(len(allFemaleFiles)/2 - 1)/float(len(allFemaleFiles)/2 + len(allMaleFiles)/2), 10)
+		classProbabilities[male] = math.log(float(len(allMaleFiles)/2)/float(len(allFemaleFiles)/2 + len(allMaleFiles)/2), 10)
 	else:
-		classProbabilities[female] = math.log(float(146)/float(292), 10)
-		classProbabilities[male] = math.log(float(145)/float(292), 10)
+		classProbabilities[female] = math.log(float(len(allFemaleFiles)/2)/float(len(allFemaleFiles)/2 + len(allMaleFiles)/2), 10)
+		classProbabilities[male] = math.log(float(len(allMaleFiles)/2 - 1)/float(len(allFemaleFiles)/2 + len(allMaleFiles)/2), 10)
 
 	# Create word probabilities for each class
 	femaleWords = {}
@@ -110,32 +131,50 @@ def trainNaiveBayes(allFiles, omitFile):
 	for file in allFiles:
 		if file != omitFile:
 			currentContent = allFiles[file]
-			if female in file:
+			# Looking at all female class
+			if 'F' in file:
 				for word in currentContent.split():
 					femaleCounter += 1
 					if word in wordProbabilities[0]:
-						wordProbabilities[0][word] += 1
+						# Checking to see if the word is a booster word
+						if word in femaleBoosterWords:
+							wordProbabilities[0][word] += booster
+						else:
+							wordProbabilities[0][word] += 1
 					else:
-						wordProbabilities[0][word] = 1
+						# Checking to see if the word is a booster word
+						if word in femaleBoosterWords:
+							wordProbabilities[0][word] = booster
+						else:
+							wordProbabilities[0][word] = 1
 						if word not in wordProbabilities[1]:
 							totalUniqueVocab += 1
+			# Looking at the male class
 			else:
 				for word in currentContent.split():
 					maleCounter += 1
 					if word in wordProbabilities[1]:
-						wordProbabilities[1][word] += 1
+						# Checking to see if the word is a booster word
+						if word in maleBoosterWords:
+							wordProbabilities[1][word] += booster
+						else:
+							wordProbabilities[1][word] += 1
 					else:
-						wordProbabilities[1][word] = 1
+						# Checking to see if the word is a booster word
+						if word in maleBoosterWords:
+							wordProbabilities[1][word] = booster
+						else:
+							wordProbabilities[1][word] = 1
 						if word not in wordProbabilities[0]:
 							totalUniqueVocab += 1
 
-	# Calculate lie probabilities
+	# Calculate female probabilities
 	for word in wordProbabilities[0]:
 		occurrence = wordProbabilities[0][word]
 		prob = float(float(occurrence + 1) / float(femaleCounter + totalUniqueVocab))
 		wordProbabilities[0][word] = math.log(prob, 10)
 
-	# Calculate truth probabilities
+	# Calculate male probabilities
 	for word in wordProbabilities[1]:
 		occurrence = wordProbabilities[1][word]
 		prob = float(float(occurrence + 1) / float(maleCounter + totalUniqueVocab))
